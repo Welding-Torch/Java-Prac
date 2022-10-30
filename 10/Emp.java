314b01272c4a0e1b3b94fc3e85cf07d3e75de39b
Welding-Torch <46340124+Welding-Torch@users.noreply.github.com>
2022-10-30 18:28:19 +0530

    10 has some problems, but uploading anyway

---
+++ 10/Emp.java
@@ -0,0 +1,81 @@
+package employee;
+import java.util.*;
+
+public class Emp
+{
+    String name,empid,category;
+    float bpay,hra,da,npay,pf,grosspay,allowance;
+    double incometax;
+	Scanner sc = new Scanner(System.in);
+    public Emp(String na,String emp,String cat,float bp,float hr,float d,float allow)
+    {
+        bpay =bp ;
+        hra=hr ;
+        da=d;
+        npay=0;
+        pf=0;
+        grosspay=0;
+        incometax=0;
+        allowance=allow;
+        name =na;
+        empid=emp;
+        category=cat;
+    }
+    public void getGross()
+    {
+        grosspay = hra+da+bpay+allowance;
+	pf=(float)0.12*grosspay;
+    }
+    public void calTax()
+    {
+        switch (category)
+        {
+            case "nc": case "NC":
+                if(grosspay<=250000) {
+                    incometax=0;
+                }
+                else if(grosspay>250000&&grosspay<=500000) {
+                    incometax=0.05*(grosspay-250000-pf);
+                } else if (grosspay>500000&&grosspay<=1000000) {
+                    incometax=12500+(0.2*(grosspay-500000-pf));
+                }
+                else {
+                    incometax=112500+(0.3*(grosspay-1000000-pf));
+                }
+                break;
+            case "sc": case "SC":
+                if(grosspay<=300000) {
+                    incometax=0;
+                }
+                else if(grosspay>300000&&grosspay<=500000) {
+                    incometax=0.05*(grosspay-300000-pf);
+                } else if (grosspay>500000&&grosspay<=1000000) {
+                    incometax=10000+(0.2*(grosspay-500000-pf));
+                }
+                else {
+                    incometax=112500+(0.3*(grosspay-1000000-pf));
+                }
+                break;
+            case "ssc": case "SSC":
+                if(grosspay<=500000) {
+                    incometax=0;
+                }
+                else if(grosspay>500000&&grosspay<=1000000) {
+                    incometax=0.2*(grosspay-pf);
+		}
+                else {
+                    incometax=100000+(0.3*(grosspay-1000000-pf));
+                }
+                break;
+                default:
+                    System.out.println("Wrong category entered");
+                    break;
+                    }
+        }
+
+    
+    public void getNet()
+    {
+        npay=grosspay-pf-(float)incometax;
+    }
+}
---
+++ 10/Emppay.java
@@ -0,0 +1,31 @@
+import employee.*;
+import java.util.*;
+class Emppay
+{
+   public static void main(String[] args)
+   {
+       Scanner sc = new Scanner(System.in);
+       String name,empid,category;
+       float bpay,hra,da,npay,pf,grosspay,allowance;
+       System.out.print("Enter the name of the Employee: ");
+       name=sc.next();
+       System.out.print("Enter the id of the Employee: ");
+       empid=sc.next();
+       System.out.print("Enter the category the Employee falls into(nc/sc/ssc): ");
+       category=sc.next();
+       System.out.print("Enter the Basic pay the employee gets:");
+       bpay=sc.nextFloat();
+       System.out.print("Enter the House rental allowance the employee gets:");
+       hra=sc.nextFloat();
+       System.out.print("Enter the Dearness allowance the employee gets:");
+       da=sc.nextFloat();
+       System.out.print("Enter the Allowance the employee gets:");
+       allowance=sc.nextFloat();
+       Emp ob = new Emp();
+       ob.getGross();
+       ob.calTax();
+       ob.getNet();
+       System.out.println("NAME\tID\tCATEGORY\tBASIC PAY\tHRA\tDA\tALLOWANCE\tGROSSPAY\tINCOMETAX\tNETPAY\tPF");
+       System.out.println(ob.name+"\t"+ob.empid+"\t"+ob.category+"\t"+ob.bpay+"\t"+ob.hraob.da+"\t"+ob.allowance+"\t"+ob.grosspay+"\t"+ob.incometax+"\t"+ob.npay+"\t"+ob.pf);
+   }
+}
