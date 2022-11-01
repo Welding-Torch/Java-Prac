df7da80236bdfcfea5193fe5345256d2868cdbf6
Welding-Torch <46340124+Welding-Torch@users.noreply.github.com>
2022-11-01 16:59:01 +0530

    Removed i unnecessary variable

--- 4.java
+++ 4.java
@@ -27,20 +27,20 @@ public class Main{
     public static void main(String[] args)
     {
 	int j;
-        Scanner sc = new Scanner (System.in);
-	System.out.println("Enter the number of students");
-	int i = sc.nextInt();
+    Scanner sc = new Scanner (System.in);
+	//System.out.println("Enter the number of students");
+	//int i = sc.nextInt();
 	Student ob =new Student();
 	System.out.print("Enter the rollno of student :");
 	int rn = sc.nextInt();
-         System.out.print("Enter the marks of student :");
-        float m = sc.nextFloat();
-        System.out.print("Enter the Score of student :");
-        float s = sc.nextInt();
-        ob.putScore(s);
+    System.out.print("Enter the marks of student :");
+    float m = sc.nextFloat();
+    System.out.print("Enter the Score of student :");
+    float s = sc.nextInt();
+    ob.putScore(s);
 	ob.putMarks(m);
 	ob.putNo(rn);
-        System.out.println("ROLL NO\tMARKS\tSCORE");
-        System.out.println(ob.rollno+"\t"+ob.marks+"\t"+ob.score);
+    System.out.println("ROLL NO\tMARKS\tSCORE");
+    System.out.println(ob.rollno+"\t"+ob.marks+"\t"+ob.score);
     }
 }
